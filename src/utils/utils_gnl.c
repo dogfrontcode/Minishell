6c41743a855ef20665f49a6113f1266cbe756924
tidos-sa <tidosrodrigues46@gmail.com>
2025-09-24 19:23:58 -0300

    adicionando utils erro, split e etc ... facilitar logica

---
+++ src/utils/utils_gnl.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_gnl.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hepple <hepple@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/17 15:56:21 by hepple            #+#    #+#             */
+/*   Updated: 2022/01/17 15:56:30 by hepple           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+ * NOTA: Esta é a implementação simplificada de get_next_line usada no minishell.
+ * É diferente da GNL padrão da 42 - lê caractere por caractere.
+ * 
+ * Você pode:
+ * 1. Usar esta versão (já está funcionando no projeto)
+ * 2. Substituir pelas chamadas da sua GNL (precisará adaptar o código)
+ */
+
+#include "global.h"
+
+static char	*minishell_gnl_free_line(char *line);
+
+char	*minishell_get_next_line(int fd)
+{
+	char	*line;
+	char	buffer;
+	int		check;
+
+	line = ft_strdup("");
+	if (line == NULL)
+		return (NULL);
+	check = read(fd, &buffer, 1);
+	if (check == -1 || check == 0)
+		return (minishell_gnl_free_line(line));
+	while (check > 0)
+	{
+		line = str_append_chr(line, buffer);
+		if (line == NULL)
+			return (NULL);
+		if (buffer == '\n')
+			return (line);
+		check = read(fd, &buffer, 1);
+	}
+	if (check == -1)
+		return (minishell_gnl_free_line(line));
+	return (line);
+}
+
+static char	*minishell_gnl_free_line(char *line)
+{
+	free(line);
+	return (NULL);
+}
